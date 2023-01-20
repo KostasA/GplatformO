46895b443121089b1c5f3fecbce17daed697e2cd
Konstantinos Anastasiou <konstantinos.anastasiou@atos.net>
2023-01-20 11:06:04 +0200

    dc

---
+++ error_handling.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+// func panic(v interface{})
+// func recover() interface
+
+func p(s string, i int) {
+	if i > 2 {
+		//panic(s)
+	}
+}
+
+func r() {
+	if err := recover(); err != nil {
+		fmt.Println(err)
+		fmt.Println("Recovered from panic")
+	}
+}
+
+func main() {
+
+	defer r()
+	p("runtime error: enter panic state", 3)
+
+	fmt.Println("End!")
+}
